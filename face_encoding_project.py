8f6c25343272cfca032a4ebc8aa9ed7be1102388
maitreyee19 <maitreyee.das85@gmail.com>
2021-04-20 01:17:34 -0700

    updated docstring

--- face_encoding_project.py
+++ face_encoding_project.py
@@ -2,61 +2,72 @@ import PIL.Image
 import PIL.ImageDraw
 import face_recognition
 import os
+
 import numpy as np
 import pandas as pd
 from pathlib import Path
-
-path = '/home/bhabani/Work/disney_character/princess1'
-path_new = '/home/bhabani/Work/disney_character/princess_cropped'
-if not os.path.exists(path_new):
-    os.mkdir(path_new)
+import cv2
 
 
-def face_location_find(file, directory):
+class PrincessFaceRecognition():
+    def __init__(self, input_path, output_path):
+        self.input_path = input_path
+        self.output_path = output_path
 
-    to_dir = directory.replace(path, path_new)
-    print("destination_dir",to_dir)
-    if not os.path.exists(to_dir):
-        os.mkdir(to_dir)
-    # print(directory)
-    image = face_recognition.load_image_file(directory + "/" + file)
-    # print(type(image))
-    face_locations = face_recognition.face_locations(image)
-    #print(face_locations)
+    def checkOpDirExists(self):
+        if not os.path.exists(self.output_path):
+            os.mkdir(self.output_path)
 
-    if len(face_locations) > 0:
-        top, right, bottom, left = face_locations[0]
+    def face_localization_crop(self, file, directory):
+        '''
 
-        image_original = PIL.Image.open(directory + "/" + file)
-        # img2 = image_original.crop((left, top, right, bottom))
-        img_name = file.split(".")[0] + "_" + ".jpeg"
-        img2 = image_original.crop((left, top, right, bottom))
+        :param file: the name of image File
+        :param directory: the directory where the image exists
+        The function calls  face_recognition.face_locations() to find four coordinates on image face
+        then image will be cropped and will be saved in a new directory named <directory>_cropped having same subdir name as input
+        '''
 
-        img2.save(to_dir+"/"+img_name,"JPEG")
-        # pil_image2 = PIL.Image.fromarray(img2)
-        # pil_image2.show()
+        to_dir = directory.replace(path, path_new)
+        print("destination_dir", to_dir)
+        if not os.path.exists(to_dir):
+            os.mkdir(to_dir)
+        # print(directory)
+        image = face_recognition.load_image_file(directory + "/" + file)
+        # print(type(image))
+        face_locations = face_recognition.face_locations(image)
+
+        if len(face_locations) > 0:
+            top, right, bottom, left = face_locations[0]
+
+            image_original = PIL.Image.open(directory + "/" + file)
+
+            img_name = file.split(".")[0] + "_" + ".jpeg"
+            img2 = image_original.crop((left, top, right, bottom))
+            rgb_im = img2.convert('RGB')
+            rgb_im.save(to_dir + "/" + img_name, "JPEG")
+            # pil_image2 = PIL.Image.fromarray(img2)
+            # pil_image2.show()
+
+    def execute_faceRecognition(self):
+        '''
+
+        :param path: takes input directory path and call face_localization_crop function with input of each image along with the directory where it exists
+        :return:
+        '''
+        self.checkOpDirExists()
+        for root, directories, files in os.walk(path):
+            for directory in directories:
+                print(directory)
+
+                # set_filetolabel(path + "/" + directory)
+            for file in files:
+                self.face_localization_crop(file, root)
 
 
-def set_filetolabel(path):
-    for root, directories, files in os.walk(path):
-        for directory in directories:
-            print(directory)
-            # set_filetolabel(path + "/" + directory)
-        for file in files:
-            face_location_find(file, root)
-            # image = face_recognition.load_image_file(file)
-            # print("image",image)
-            # face_locations = face_recognition.face_locations(image)
-            # print(face_locations)
-            # pil_image = PIL.Image.fromarray(image)
-            # print("pil_image",pil_image)
-            # for face_location in face_locations:
-            #
-            #     top, right, bottom, left = face_location
-            #     draw = PIL.ImageDraw.Draw(pil_image)
-            #     draw.rectangle([left, top, right, bottom], outline="red")
+# read_imageFile(path)
 
-            # pil_image.show()
-
-
-set_filetolabel(path)
+if __name__ == "__main__":
+    path = '/home/bhabani/Work/disney_character/princess'
+    path_new = '/home/bhabani/Work/disney_character/princess_cropped'
+    princessCharacters = PrincessFaceRecognition(path, path_new)
+    princessCharacters.execute_faceRecognition()
